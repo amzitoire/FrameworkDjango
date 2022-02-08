2403eede97839095db2d3a1bb976535e3e413aff
amzitoire <amzidou16@gmail.com>
2022-02-08 09:27:58 +0000

    crispy form

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -1,13 +1,14 @@
 from email.mime import image
 from django import forms
 
+
 from django.conf import settings
 
 
 # form person
 
 class PersonForm(forms.Form):
-    name = forms.CharField( required=True, max_length=255,strip=True,min_length=2,widget=forms.TextInput(attrs={'type': 'text'}))
+    name = forms.CharField( required=True,label="Name", max_length=255,strip=True,min_length=2,widget=forms.TextInput(attrs={'type': 'text'}))
     sex = forms.ChoiceField(required=True,choices=[(x,y) for(x,y) in settings.SEXES],widget=forms.Select(attrs={'type': 'select'}))
     country = forms.ChoiceField(required=True,choices=[(x,y) for(x,y) in settings.COUNTRIES],widget=forms.Select(attrs={'type': 'select'}))
     age = forms.CharField( required=True, max_length=255,strip=True,min_length=2,widget=forms.TextInput(attrs={'type': 'number'}))
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-
 from polls.models import *
 from polls.forms import *
 
